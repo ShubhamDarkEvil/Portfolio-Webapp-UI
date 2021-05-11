af5f9374978b3c75091c02325489f198a659a7ee
Shubham Prakash <prakashshubham36@gmail.com>
2021-05-11 21:41:16 +0530

    added orientation support & fixed mobile display overflow

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -18,6 +18,17 @@ export class LandingComponent implements OnInit {
   ngOnInit(): void {
     this.invokeParticles();
 
+    let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
   }
   public invokeParticles(): void {
     particlesJS('particles-js', ParticlesConfig, function() {});
